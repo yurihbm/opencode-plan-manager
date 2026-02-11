77c4caa5f45a85508ccd1e28a74cb17799dd9670
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-02-11 15:46:19 -0300

    refactor(prompts): use less tokens on custom prompts
    System reminder was inserted with the PLAM_PROMPT.
    Both PLAN_PROMPT and BUILD_PROMPT were improved to use less tokens.
    If user configured custom prompts for Plan or Build agents, plugins
    prompts are not used.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import type { Plugin } from "@opencode-ai/plugin";
 import PLAN_PROMPT from "./prompts/plan.txt";
 import BUILD_PROMPT from "./prompts/build.txt";
-import PLAN_SYSTEM_REMINDER from "./system/plan.txt";
 import { planCreate, planList, planRead, planUpdate } from "./tools";
 
 const SESSION_AGENT_MAP = new Map<string, string>();
@@ -49,26 +48,15 @@ export const OpenPlanManagerPlugin: Plugin = async ({ client }) => {
         }
       }
     },
-    "experimental.chat.system.transform": async (input, output) => {
-      if (input.sessionID && SESSION_AGENT_MAP.has(input.sessionID)) {
-        const agent = SESSION_AGENT_MAP.get(input.sessionID);
-
-        switch (agent) {
-          case "plan":
-            output.system.splice(0, output.system.length, PLAN_SYSTEM_REMINDER);
-            break;
-        }
-      }
-    },
     config: async (input) => {
-      if (input.agent?.plan) {
+      if (input.agent?.plan && !input.agent.plan.prompt) {
         input.agent.plan = {
           ...input.agent.plan,
           prompt: PLAN_PROMPT,
         };
       }
 
-      if (input.agent?.build) {
+      if (input.agent?.build && !input.agent.build.prompt) {
         input.agent.build = {
           ...input.agent.build,
           prompt: BUILD_PROMPT,
